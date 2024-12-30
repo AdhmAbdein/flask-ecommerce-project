a66e49bc82823967d7a6e96b21c3b39c217b0532
root <root@master.localdomain>
2024-12-31 00:37:25 +0100

    initial commit

--- project-code/config.py
+++ project-code/config.py
@@ -6,17 +6,3 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
-# Define your models
-class Product(db.Model):
-    __tablename__ = 'product'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    price = db.Column(db.Float)
-    description = db.Column(db.String(255))
-
-# Create tables in the database
-with app.app_context():
-    db.create_all()
-
-if __name__ == "__main__":
-    app.run()
