fd0aaa5344a8eb7224f964205116c8b3816b04b9
root <root@master.localdomain>
2024-12-31 00:32:47 +0100

    initial commit

--- project-code/config.py
+++ project-code/config.py
@@ -5,3 +5,18 @@ class Config:
     SQLALCHEMY_DATABASE_URI = "postgresql://adham:1234@postgres-service-ecommerce/ecommerce_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+
+# Define your models
+class Product(db.Model):
+    __tablename__ = 'product'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    price = db.Column(db.Float)
+    description = db.Column(db.String(255))
+
+# Create tables in the database
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run()
