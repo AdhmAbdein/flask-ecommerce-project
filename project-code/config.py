038152e10597ec13e4773069a893b03ce8a0a772
root <root@master.localdomain>
2024-12-31 00:43:56 +0100

    initial commit

--- project-code/config.py
+++ project-code/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = "postgresql://adham:1234@postgres-service-ecommerce/ecommerce_db"
+    SQLALCHEMY_DATABASE_URI = "postgresql://adham:1234@postgres-service-ecommerce:5432/ecommerce_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
